9f5ab66f11639e4b7a65c8f839c9c35c83eee069
Måns Alskog <mans.alskog@liu.se>
2023-06-21 20:07:46 +0200

    Consider climb in route, added ETO

--- aircraft_data.js
+++ aircraft_data.js
@@ -3,9 +3,7 @@ const aircraft_data = {
     'SE-MMZ': {
         'cruise_speed': 105, // at sea level, 55% power, 15 degrees OAT
         'fuel_consumption': us_gal_to_l(11.0), // for 75% power, best power mixture
-        'atc_equipment': '/S',
-        'atc_other_info': '',
-        'atc_survival_info': 'R/E A/WR', // ELT available, no portable UHF/VHF, aircraft colour white and red
+        'climb_vertical_speed': 520, // fpm, at 4000 ft pressure alt., ISA
         'bew': lb_to_kg(1783.48),
         'max_fuel_volume': us_gal_to_l(48),
         'arm': {
@@ -15,13 +13,14 @@ const aircraft_data = {
             'Baggage': in_to_cm(142.8),
             'Fuel': in_to_cm(95.0),
         },
+        'atc_equipment': '/S',
+        'atc_other_info': '',
+        'atc_survival_info': 'R/E A/WR', // ELT available, no portable UHF/VHF, aircraft colour white and red
     },
     'SE-LRL': {
         'cruise_speed': 105, // at sea level, 55% power, 15 degrees OAT
         'fuel_consumption': us_gal_to_l(11.0), // for 75% power, best power mixture
-        'atc_equipment': 'BDFGLORY/S',
-        'atc_other_info': 'PBN/D2S1',
-        'atc_survival_info': 'R/E A/WB', // ELT available, no portable UHF/VHF, aircraft colour white and blue
+        'climb_vertical_speed': 520, // fpm, at 4000 ft pressure alt., ISA
         'bew': lb_to_kg(1590.0), // TODO double check this
         'max_fuel_volume': us_gal_to_l(0),
         'arm': {
@@ -31,13 +30,31 @@ const aircraft_data = {
             'Baggage': in_to_cm(142.8),
             'Fuel': in_to_cm(95.0),
         },
+        'atc_equipment': 'BDFGLORY/S',
+        'atc_other_info': 'PBN/D2S1',
+        'atc_survival_info': 'R/E A/WB', // ELT available, no portable UHF/VHF, aircraft colour white and blue
     },
     'SE-IUD': {
         'cruise_speed': 95, // at sea level, 55% power, 15 degrees OAT
         'fuel_consumption': us_gal_to_l(10.5), // for 75% power, best power mixture
+        'climb_vertical_speed': 530, // fpm, at 4000 ft pressure alt., ISA
+        'bew': lb_to_kg(1607.2),
+        'max_fuel_volume': us_gal_to_l(48),
+        'arm': {
+            'BEW': in_to_cm(87.4),
+            'Front seats': in_to_cm(80.5),
+            'Rear seats': in_to_cm(118.1),
+            'Baggage': in_to_cm(142.8),
+            'Fuel': in_to_cm(95.0),
+        },
         'atc_equipment': 'BDFGLORY/S',
         'atc_other_info': 'PBN/D2S1',
         'atc_survival_info': 'R/E A/WR', // ELT available, no portable UHF/VHF, aircraft colour white and red
+    },
+    'SE-KIT': { // NOTE: SE-KIT uses same data as SE-IUD (except for atc_ fields), full POH not avail.
+        'cruise_speed': 95, // at sea level, 55% power, 15 degrees OAT
+        'fuel_consumption': us_gal_to_l(10.5), // for 75% power, best power mixture
+        'climb_vertical_speed': 530, // fpm, at 4000 ft pressure alt., ISA
         'bew': lb_to_kg(1607.2),
         'max_fuel_volume': us_gal_to_l(48),
         'arm': {
@@ -47,29 +64,14 @@ const aircraft_data = {
             'Baggage': in_to_cm(142.8),
             'Fuel': in_to_cm(95.0),
         },
-    },
-    'SE-KIT': { // NOTE: SE-KIT uses same data as SE-IUD, full POH not avail.
-        'cruise_speed': 95, // at sea level, 55% power, 15 degrees OAT
-        'fuel_consumption': us_gal_to_l(10.5), // for 75% power, best power mixture
         'atc_equipment': 'BDFGLORY/S',
         'atc_other_info': 'PBN/D2S1',
         'atc_survival_info': 'R/E A/WB', // ELT available, no portable UHF/VHF, aircraft colour white and blue
-        'bew': lb_to_kg(1607.2),
-        'max_fuel_volume': us_gal_to_l(48),
-        'arm': {
-            'BEW': in_to_cm(87.4),
-            'Front seats': in_to_cm(80.5),
-            'Rear seats': in_to_cm(118.1),
-            'Baggage': in_to_cm(142.8),
-            'Fuel': in_to_cm(95.0),
-        },
     },
     'SE-KMH': {
         'cruise_speed': 90, // at sea level, 55% power, 15 degrees OAT
         'fuel_consumption': us_gal_to_l(10), // for 75% power, best power mixture
-        'atc_equipment': 'DFGLORY/S',
-        'atc_other_info': 'PBN/D2S1',
-        'atc_survival_info': 'R/E A/WB', // ELT available, no portable UHF/VHF, aircraft colour white and blue
+        'climb_vertical_speed': 360, // from POH figure 5-17, seems very low? TODO check again
         'bew': lb_to_kg(1464.9),
         'max_fuel_volume': us_gal_to_l(48),
         'arm': {
@@ -79,13 +81,14 @@ const aircraft_data = {
             'Baggage': in_to_cm(142.8),
             'Fuel': in_to_cm(95.0),
         },
+        'atc_equipment': 'DFGLORY/S',
+        'atc_other_info': 'PBN/D2S1',
+        'atc_survival_info': 'R/E A/WB', // ELT available, no portable UHF/VHF, aircraft colour white and blue
     },
     'SE-EMW': {
         'cruise_speed': mph_to_kt(134), // at sea level, 75% power, ISA OAT (55% power not avail.)
         'fuel_consumption': us_gal_to_l(10.0), // for 75% power, best power mixture
-        'atc_equipment': 'not avail.',
-        'atc_other_info': 'not avail.',
-        'atc_survival_info': 'R/E A/WB', // ELT available, no portable UHF/VHF, aircraft colour white and blue
+        'climb_vertical_speed': 600, // from POH page 22, seems very high? TODO check again
         'bew': 648.9,
         'max_fuel_volume': us_gal_to_l(50),
         'arm': {
@@ -94,6 +97,9 @@ const aircraft_data = {
             'Rear seats': 300.0,
             'Baggage': 362.7,
             'Fuel': 241.3,
-        }
+        },
+        'atc_equipment': 'not avail.',
+        'atc_other_info': 'not avail.',
+        'atc_survival_info': 'R/E A/WB', // ELT available, no portable UHF/VHF, aircraft colour white and blue
     },
 };
--- main.js
+++ main.js
@@ -9,20 +9,42 @@ function ground_speed(wind_angle, wind_speed, true_airspeed, true_track) {
     return true_airspeed + wind_speed * Math.cos(deg_to_rad(inc_angle));
 }
 
-function calc_route_info(wind_angle, wind_speed, variation, true_airspeed, fuel_consumption, route) {
+// Convert a number of milliseconds since the Unix epoch to a time on format hh:mm.
+function epoch_ms_to_time(epoch_ms) {
+    const date = new Date(epoch_ms);
+    return date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0');
+}
+
+function calc_route_info(wind_angle, wind_speed, variation, true_airspeed, fuel_consumption, climb_speed, date_of_flight, departure_time, route) {
     let route_info = [];
+    let ms_since_epoch = new Date(date_of_flight + 'T' + departure_time).getTime();
     for (let i = 0; i < route.length; i++) {
         const wca = wind_correction_angle(wind_angle, wind_speed, true_airspeed, route[i].true_track);
         const th = route[i].true_track + wca;
         const gs = ground_speed(wind_angle, wind_speed, true_airspeed, route[i].true_track);
-        const tol_h = route[i].distance / gs;
+        const travel_hours = route[i].distance / gs;
+        // If we're on the first leg, or we have a positive altitude change,
+        // then we need to take into account the time needed to climb.
+        // NOTE: It's not mathematically sound to simply add the times like this,
+        // but for a first approximation it works, especially since the values are low.
+        let climb_mins = 0;
+        if (i === 0) {
+            climb_mins = route[i].altitude / climb_speed;
+        } else if (route[i - 1].altitude < route[i].altitude) {
+            climb_mins = (route[i].altitude - route[i - 1].altitude) / climb_speed;
+        }
+        const tol_mins = travel_hours * 60 + climb_mins;
+        // Strange way to keep track of the time, due to JS missing an addMinutes() function on Date objects.
+        ms_since_epoch += tol_mins * 60 * 1000;
         route_info.push({
             wind_correction_angle: wca,
             true_heading: th,
             compass_heading: th - variation,
             ground_speed: gs,
-            time_on_leg: tol_h * 60,
-            fuel_required: tol_h * fuel_consumption,
+            time_on_leg: tol_mins,
+            fuel_required: travel_hours * fuel_consumption,
+            climb_time: climb_mins,
+            estimated_time_over: epoch_ms_to_time(ms_since_epoch),
         });
     }
     return route_info;
@@ -37,8 +59,38 @@ function calc_atc_route(route) {
     return atc_route;
 }
 
-function minutes_to_eet_format(minutes) {
-    return (minutes / 60).toFixed(0).padStart(2, '0') + (minutes % 60).toFixed(0).padStart(2, '0');
+// Get current date on YYYY-MM-DD format, in the local timezone (i.e. cannot use toISOString)
+function current_local_date() {
+    const date = new Date();
+    return date.getFullYear().toString()
+        + '-' + (date.getMonth() + 1).toString().padStart(2, '0')
+        + '-' + date.getDate().toString().padStart(2, '0');
+}
+
+// Convert a date on format YYYY-MM-DD to a date on format YYMMDD
+function to_atc_date(datestr) {
+    const date = new Date(datestr);
+    return date.getFullYear().toString().slice(2)
+        + (date.getMonth() + 1).toString().padStart(2, '0')
+        + date.getDate().toString().padStart(2, '0');
+}
+
+// Convert a local date and time to an UTC time on the form hhmm
+function to_atc_departure_time(datestr, timestr) {
+    const date = new Date(datestr + 'T' + timestr);
+    console.log(date);
+    return date.getUTCHours().toString().padStart(2, '0') + date.getUTCMinutes().toString().padStart(2, '0');
+}
+
+function to_atc_eet(route_mins) {
+    return (route_mins / 60).toFixed(0).padStart(2, '0') + (route_mins % 60).toFixed(0).padStart(2, '0');
+}
+
+// Very hacky heuristic for ICAO location indicators. TODO improve this.
+function is_icao_airport(name) {
+    // If length is 4 and prefix is Sweden, Finland, Denmark, or Norway
+    const country_prefix = ['ES', 'EF', 'EK', 'EN'];
+    return name.length === 4 && country_prefix.some((p) => name.startsWith(p));
 }
 
 function weight_of_9196ul(volume) {
@@ -55,13 +107,19 @@ createApp({
         const wind_angle = ref(90); // NOTE: example value
         const wind_speed = ref(15); // NOTE: example value
         const variation = ref(7); // NOTE: example value
+
+        const date_of_flight = ref(current_local_date());
+        const departure_time = ref('12:00');
+        const cruise_altitude = ref(4500);
+
         const route = ref([
-            {from: 'ESSL', true_track: 170, distance: 6, to: 'VSN'},
-            {from: 'VSN', true_track: 350, distance: 6, to: 'ESSL'},
+            {from: 'ESSL', true_track: 170, distance: 6, to: 'VSN', altitude: 1500},
+            {from: 'VSN', true_track: 350, distance: 12, to: 'MOTALA', altitude: 3500},
         ]);
         const route_info = computed(() =>
             calc_route_info(wind_angle.value, wind_speed.value, variation.value,
-                aircraft.value.cruise_speed, aircraft.value.fuel_consumption,
+                aircraft.value.cruise_speed, aircraft.value.fuel_consumption, aircraft.value.climb_vertical_speed,
+                date_of_flight.value, departure_time.value,
                 route.value));
         const time_on_route = computed(() =>
             route_info.value.map((leg) => leg.time_on_leg).reduce((t, acc) => t + acc, 0));
@@ -107,10 +165,19 @@ createApp({
         const ramp_arm = computed(() => ramp_moment.value / ramp_weight.value);
 
         /* ATC flight plan */
-        const departure_time = ref('12:00');
-        const cruise_level = ref(4500);
         const atc_route = computed(() => calc_atc_route(route.value));
-        const formatted_eet = computed(() => minutes_to_eet_format(time_on_route.value));
+        const atc_departure_time = computed(() => to_atc_departure_time(date_of_flight.value, departure_time.value));
+        const atc_route_eet = computed(() => to_atc_eet(time_on_route.value));
+        const atc_departure = computed(() =>
+            is_icao_airport(route.value[0].from) ? route.value[0].from : 'ZZZZ');
+        const atc_destination = computed(() =>
+            is_icao_airport(route.value[route.value.length - 1].to) ? route.value[route.value.length - 1].to : 'ZZZZ');
+        const atc_other_departure = computed(() =>
+            is_icao_airport(route.value[0].from) ? '' : ' DEP/' + route.value[0].from);
+        const atc_other_destination = computed(() =>
+            is_icao_airport(route.value[route.value.length - 1].to) ? '' : ' DEST/' + route.value[route.value.length - 1].to);
+        const atc_date_of_flight = computed(() =>
+            date_of_flight.value === current_local_date() ? '' : ' DOF/' + to_atc_date(date_of_flight.value));
 
         return {
             reg,
@@ -119,6 +186,11 @@ createApp({
             wind_angle,
             wind_speed,
             variation,
+
+            date_of_flight,
+            departure_time,
+            cruise_altitude,
+
             route,
             route_info,
             time_on_route,
@@ -137,10 +209,14 @@ createApp({
             dry_arm,
             ramp_arm,
 
-            departure_time,
-            cruise_level,
             atc_route,
-            formatted_eet,
+            atc_departure_time,
+            atc_route_eet,
+            atc_departure,
+            atc_destination,
+            atc_other_departure,
+            atc_other_destination,
+            atc_date_of_flight,
         };
     },
 }).mount('#app');
